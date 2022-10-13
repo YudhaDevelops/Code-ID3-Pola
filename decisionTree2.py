6597cfa69dc4eb003f41da0d40377affe4b88edf
boim212 <bimaboim7@gmail.com>
2022-10-13 15:37:11 +0700

    update

--- decisionTree2.py
+++ decisionTree2.py
@@ -15,63 +15,75 @@ hasil = [];
 def klasifikasi(bentuk, warnaDaun, lebar, teksDaun, tulDaun):
     
     if bentuk == 1:
-        #hasil.append("Daun Sirih")
+        hasil.append("Daun Sirih")
         print("Daun Sirih")
     elif bentuk == 8:
-        #hasil.append("Daun Singkong")
+        hasil.append("Daun Singkong")
         print("Daun Singkong")
     elif bentuk == 11 :
-        #hasil.append("Daun Paku")
+        hasil.append("Daun Paku")
         print("Daun Paku")
     elif bentuk == 15 :
-        #hasil.append("Daun Nangka")
+        hasil.append("Daun Nangka")
         print("Daun Nangka")
     elif bentuk == 17 :
-        #hasil.append("Daun Ketapang")
+        hasil.append("Daun Ketapang")
         print("Daun Ketapang")
     elif bentuk == 23 :
-        #hasil.append("Daun Palem")
+        hasil.append("Daun Palem")
         print("Daun Palem")
     elif bentuk == 3 :
         if  warnaDaun == 1 :
-            #hasil.append("Daun Bayam Liar")
+            hasil.append("Daun Bayam Liar")
             print("Daun Bayam Liar")
         elif warnaDaun == 2 :
-            #hasil.append("Daun Zamioculcas")
+            hasil.append("Daun Zamioculcas")
             print("Daun Zamioculcas")
+        else:
+            hasil.append('Data Tidak Dikenali')
+            print('Data Tidak Dikenali')
     elif bentuk == 4 :
         if teksDaun == 0 :
-            #hasil.append("Kamboja")
+            hasil.append("Kamboja")
             print("Kamboja")
         elif teksDaun == 2 :
-            #hasil.append("Rambutan")
+            hasil.append("Rambutan")
             print("Rambutan")
+        else:
+            hasil.append('Data Tidak Dikenali')
+            print('Data Tidak Dikenali')
     elif bentuk == 6 :
         if lebar == 1 or lebar == 3 :
-            #hasil.append("Daun Jambu")
+            hasil.append("Daun Jambu")
             print("Daun Jambu")
         elif lebar == 2 :
             if tulDaun == 0 :
-                #hasil.append("Tanjung")
+                hasil.append("Tanjung")
                 print("Tanjung")
             elif tulDaun == 1 :
-                #hasil.append("Daun Jambu")
+                hasil.append("Daun Jambu")
                 print("Daun Jambu")
+            else:
+                print('Data Tidak Dikenali')
         elif lebar == 4 or lebar == 5 :
-            #hasil.append("Mangga")
+            hasil.append("Mangga")
             print("Mangga")
+        else:
+            hasil.append('Data Tidak Dikenali')
+            print('Data Tidak Dikenali')
     elif bentuk == 18 :
         if tulDaun == 0 :
-            #hasil.append("SunFlower")
+            hasil.append("SunFlower")
             print("SunFlower")
         elif tulDaun == 2:
-            #hasil.append("Bambu");
+            hasil.append("Bambu");
             print("Daun Bambu")
         else:
-            #hasil.append("Daun tidak dikenali")
+            hasil.append("Daun tidak dikenali")
             print("Daun tidak dikenali")
     else:
         print('Data Tidak Dikenali')
+        hasil.append('Data Tidak Dikenali')
         
 
 for i in range(0,jum_data_test):
@@ -92,5 +104,5 @@ for i in range(0,jum_data_test):
     klasifikasi(test_bentuk, test_warna, test_lebar, test_teksDaun, test_tulDaun)
     
     
-#data_test["Hasil Akhir"] = hasil
+data_test["Hasil Akhir"] = hasil
 #print(data_test)
